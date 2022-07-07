00213161452eb866f3ff5b543f776e8b650c6e85
Skand Sharma <92265662+skand1110@users.noreply.github.com>
2022-07-08 02:11:38 +0530

    Add files via upload

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar mb-5">
+      <span className="navbar-brand mb-0 h1 mx-auto">LyricsFinder</span>
+    </nav>
+  );
+};
+
+export default Navbar;
